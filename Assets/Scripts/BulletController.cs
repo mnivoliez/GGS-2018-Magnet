a0d830c8d27ec7105a6b2d06a2c235e2491b71a9
Unknown <gomez.gwendo@gmail.com>
2018-01-27 00:17:22 +0100

    BulletReceiver : absorb bullets when it collide and destroy it when in center

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+	private Rigidbody body;
+	[SerializeField] private Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody>();
+		body.AddForce(direction.normalized * 100);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.position = new Vector3(transform.position.x + 0.05f, transform.position.y, transform.position.z);
+	}
+}
---
+++ Assets/Scripts/TestsGwen/AbsorbtionController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbsorbtionController : MonoBehaviour {
+	
+	private List<GameObject> objectsInAbsorbtion;
+	[SerializeField]private float rangeDestroy = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+		objectsInAbsorbtion = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (objectsInAbsorbtion.Count > 0)
+		{
+			List<GameObject> objectsToDestroy = new List<GameObject>();
+
+			foreach (GameObject objectToDestroy in objectsInAbsorbtion)
+			{
+				if ((objectToDestroy.transform.position - transform.position).magnitude <= rangeDestroy)
+				{
+					objectsToDestroy.Add(objectToDestroy);
+				}
+			}
+			
+			if (objectsToDestroy.Count > 0)
+			{
+				foreach (GameObject objectToDestroy in objectsToDestroy)
+				{
+					objectsInAbsorbtion.Remove(objectToDestroy);
+					Destroy(objectToDestroy);
+				}
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		GameObject objectCollided = collider.gameObject;
+		if (objectCollided.tag == "Bullet")
+		{
+			Rigidbody body = objectCollided.GetComponent<Rigidbody>();
+			body.velocity = (transform.position - objectCollided.transform.position).normalized;
+			objectsInAbsorbtion.Add(objectCollided);
+		}
+	}
+}
