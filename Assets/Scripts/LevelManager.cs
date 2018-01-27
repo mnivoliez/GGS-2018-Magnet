4326e0b63259bee3a923fbb49d02151e96403687
Mathieu Nivoliez <mathieu.nivoliez@laposte.net>
2018-01-27 23:17:42 +0100

    add level manager. Should work on mainmenu next. The game will be quit from there.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+using System;
+
+public class LevelManager : MonoBehaviour
+{
+	private int currentLevel;
+
+	private List<string> scenesInBuild;
+
+	void Start()
+	{
+		scenesInBuild = new List<string>();
+		for (int i = 1; i < SceneManager.sceneCountInBuildSettings; i++)
+		{
+			string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+			int lastSlash = scenePath.LastIndexOf("/");
+			scenesInBuild.Add(scenePath.Substring(lastSlash + 1, scenePath.LastIndexOf(".") - lastSlash - 1));
+		}
+
+        currentLevel = PlayerPrefs.GetInt("currentLevel", 0);
+	}
+
+	public void LoadMainMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void LoadNextLevel()
+	{
+		currentLevel++;
+        PlayerPrefs.SetInt("currentLevel", currentLevel);
+        SceneManager.LoadScene("Level_"+currentLevel);
+	}
+
+	public bool IsItLastLevel()
+	{
+		return !(scenesInBuild.Contains ("Level_" + (currentLevel + 1)));
+	}
+}
