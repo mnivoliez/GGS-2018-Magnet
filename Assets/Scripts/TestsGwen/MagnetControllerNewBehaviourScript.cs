097fb3460abb961c8f584b19d9b56cb9f60d0f9f
Unknown <gomez.gwendo@gmail.com>
2018-01-27 06:57:46 +0100

    Change MAgnetControlly : now the material change dinamically

--- Assets/Scripts/TestsGwen/MagnetControllerNewBehaviourScript.cs
+++ Assets/Scripts/TestsGwen/MagnetControllerNewBehaviourScript.cs
@@ -9,12 +9,14 @@ enum MagnetChild {
 public class MagnetControllerNewBehaviourScript : MonoBehaviour {
 
 	private MagnetEffect magnetEffect;
-
+	[SerializeField] private DirectionForce directionForce;
+	[SerializeField] private float intensity = 2;
 
 	// Use this for initialization
 	void Start () {
 		magnetEffect = transform.Find(MagnetChild.AreaEffect.ToString()).gameObject.GetComponent<MagnetEffect>();
-
+		magnetEffect.DirectionForce = this.directionForce;
+		magnetEffect.Intensity = this.intensity;
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/TestsGwen/MagnetEffect.cs
+++ Assets/Scripts/TestsGwen/MagnetEffect.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-enum DirectionForce
+public enum DirectionForce
 {
 	Attract, Repulse
 };
@@ -10,12 +10,37 @@ enum DirectionForce
 public class MagnetEffect : MonoBehaviour {
 
 	private List<GameObject> objectsInRange;
-	[SerializeField] private DirectionForce directionForce;
-	[SerializeField] private float intensity = 2;
+	private DirectionForce directionForce;
+	private float intensity;
+	private Material attractMaterial;
+	private Material repulseMaterial;
+	private MeshRenderer meshRend;
+	private Material[] mats;
 
 	// Use this for initialization
 	void Start () {
 		objectsInRange = new List<GameObject>();
+		meshRend = GetComponent<MeshRenderer>();
+		attractMaterial = Resources.Load("RedTransparent") as Material;
+		repulseMaterial = Resources.Load("BlueTransparent") as Material;
+		mats = GetComponent<Renderer>().materials;
+		mats[0] = attractMaterial;
+		GetComponent<Renderer>().materials = mats;
+	}
+
+
+	void Update()
+	{
+		if (mats[0].name != "RedTransparent" && directionForce == DirectionForce.Attract)
+		{
+			mats[0] = attractMaterial;
+			GetComponent<Renderer>().materials = mats;
+		}
+		else if (mats[0].name != "BlueTransparent" && directionForce == DirectionForce.Repulse)
+		{
+			mats[0] = repulseMaterial;
+			GetComponent<Renderer>().materials = mats;
+		}
 	}
 	
 	// Update is called once per frame
@@ -60,4 +85,28 @@ public class MagnetEffect : MonoBehaviour {
 			directionForce == DirectionForce.Attract ? 
 				DirectionForce.Repulse : DirectionForce.Attract;
 	}
+
+	public DirectionForce DirectionForce
+    {
+        get
+        {
+            return directionForce;
+        }
+        set
+        {
+            directionForce = value;
+        }
+    }
+
+	public float Intensity
+    {
+        get
+        {
+            return intensity;
+        }
+        set
+        {
+            intensity = value;
+        }
+    }
 }
