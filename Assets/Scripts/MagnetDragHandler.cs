656930d4a2ef71532d0e9802c6ce5e584e8f2cce
Unknown <gomez.gwendo@gmail.com>
2018-01-27 08:27:53 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/MagnetDragHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetDragHandler : MonoBehaviour {
+
+	private Vector3 dist;
+	private float posX;
+	private float posY;
+
+	void OnMouseDown()
+	{
+		dist = Camera.main.WorldToScreenPoint(transform.position);
+		posX = Input.mousePosition.x - dist.x;
+		posY = Input.mousePosition.y - dist.y;
+	}
+
+	void OnMouseDrag()
+	{
+		Vector3 curPos = new Vector3(Input.mousePosition.x - posX, Input.mousePosition.y - posY, dist.z);
+		Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
+		transform.parent.position = worldPos;
+	}
+
+}
