af287c86a8e8ef063a341ddf854f2e254810ec81
Mathieu Nivoliez <mathieu.nivoliez@laposte.net>
2018-01-27 07:27:47 +0100

    Add gamemanager

--- Assets/Scripts/BulletLauncherController.cs
+++ Assets/Scripts/BulletLauncherController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletLauncherController : MonoBehaviour {
+	[SerializeField] private GameManager _gameManager;
 	[SerializeField] private Transform _bulletOutput;
 	[SerializeField] private Rigidbody _bulletPrefab;
-	[SerializeField] [Range(-90, 90)] private float _angle;
+	[SerializeField] [Range(-90, 90)] private List<float> _angles;
+
 	[SerializeField] private float _intensity;
 
 	[SerializeField] private float _bulletRate;
@@ -14,6 +16,7 @@ public class BulletLauncherController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		_timeSinceLastShot = 0f;
+		_gameManager.PlanShotsNumber(_angles.Count);
 	}
 	
 	// Update is called once per frame
@@ -22,7 +25,7 @@ public class BulletLauncherController : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
-		if (_timeSinceLastShot > _bulletRate) {
+		if (_angles.Count > 0 && _timeSinceLastShot > _bulletRate) {
 			_timeSinceLastShot = 0f;
 			LaunchBullet();
 		} else {
@@ -31,11 +34,12 @@ public class BulletLauncherController : MonoBehaviour {
 	}
 
 	private void LaunchBullet () {
-		
-		transform.rotation = Quaternion.Euler(0, 0, _angle -90);
+		transform.rotation = Quaternion.Euler(0, 0, _angles[0] -90);
 		Rigidbody bullet = Instantiate(_bulletPrefab, _bulletOutput.position, Quaternion.identity);
 		Vector3 direction = _bulletOutput.transform.position - transform.position;
 		direction.Normalize();
 		bullet.AddForce(direction * _intensity, ForceMode.Force);
+		_angles.RemoveAt(0);
+		_gameManager.LaunchedBullet();
 	}
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	private int _launch;
+	private int _received;
+
+	private int _shotsNumber;
+
+	[SerializeField] private float _timeout;
+	private float _effectiveTimeout;
+	private bool _startTimeout;
+
+	// Use this for initialization
+	void Start () {
+		_launch = 0;
+		_received = 0;
+		_shotsNumber = 0;
+		_effectiveTimeout = 0f;
+		_startTimeout = false;
+	}
+
+	void FixedUpdate() {
+		if(_startTimeout) {
+			_effectiveTimeout += Time.fixedDeltaTime;			
+		}
+		if(_received != _shotsNumber && _effectiveTimeout > _timeout) {
+			Lose();
+		}
+		if(_received == _shotsNumber && _effectiveTimeout < _timeout) {
+			Win();
+		}
+	}
+	
+	public void PlanShotsNumber (int number) {
+		_shotsNumber = number;
+	}
+
+	public void LaunchedBullet() {
+		_launch++;
+		if (_launch == _shotsNumber) {
+			_startTimeout = true;
+		}
+	}
+
+	public void ReceivedBullet () {
+		_received++;
+	}
+
+	private void Win() {
+		Debug.Log("winner");
+
+	}
+
+	private void Lose() {
+		Debug.Log("loser");
+	}
+}
